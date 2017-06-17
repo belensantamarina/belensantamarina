40389c0a92abe1346def746139a50d0a22e729e3
Andres Moritan <amoritan@me.com>
2017-06-17 12:41:14 -0300

    modal gallery ready

--- javascripts/index.js
+++ javascripts/index.js
@@ -6,7 +6,41 @@ $(function() {
     scrollBar: true,
     controlArrows: false,
     verticalCentered: true
-    // fadingEffect: 'slides'
+  });
+  
+  
+  
+  var fotorama;
+  
+  $('.open-modal').click(function(e) {
+    var target = $(this).attr('href');
+    
+    $('#fotorama').html($(target).html());
+    
+    $('#modal_gallery').fadeIn(400, function() {
+      var fotorama_target = $('#fotorama').fotorama();
+      fotorama = fotorama_target.data('fotorama');
+      
+      $('#fotorama').animate({ opacity: 1 }, 400);
+    });
+    
+    $.fn.fullpage.setAllowScrolling(false);
+    $('html').css('height', '100%').css('overflow', 'hidden');
+    
+    e.preventDefault();
+  });
+  
+  $('#modal_close').click(function(e) {
+    $.fn.fullpage.setAllowScrolling(true);
+    $('html').css('height', 'auto').css('overflow', 'auto');
+    
+    $('#fotorama').animate({ opacity: 0 }, 400, function() {
+      $('#modal_gallery').fadeOut(400, function() {
+        fotorama.destroy();
+      });
+    });
+    
+    e.preventDefault();
   });
   
 });
