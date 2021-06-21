e8adb7f5a6e8a63d7f4b57654d7ec0835e94e959
Andres Moritan <amoritan@me.com>
2021-06-21 22:39:16 +0100

    prettier installed

--- render/index.js
+++ render/index.js
@@ -3,22 +3,23 @@ const mustache = require('mustache');
 const { readDirectory, readFile, writeFile } = require('./utils/filesHandler');
 
 const render = async () => {
-	let baseTemplate = await readFile('index.html');
-	let websiteConstants = await readFile('content/english_constants.json', true);
-	let websiteMenu = await readFile('content/english_menu.json', true);
+  let baseTemplate = await readFile('index.html');
+  let websiteConstants = await readFile('content/english_constants.json', true);
+  let websiteMenu = await readFile('content/english_menu.json', true);
 
-	const navItems = websiteMenu.items.map((itemString) => (
-		{ name: itemString.split('|')[0], reference: itemString.split('|')[1] }
-	));
+  const navItems = websiteMenu.items.map((itemString) => ({
+    name: itemString.split('|')[0],
+    reference: itemString.split('|')[1],
+  }));
 
-	templateData = {
-		...websiteConstants,
-		nav_items: navItems,
-	}
+  templateData = {
+    ...websiteConstants,
+    nav_items: navItems,
+  };
 
-	const templateOutput = mustache.render(baseTemplate, templateData);
+  const templateOutput = mustache.render(baseTemplate, templateData);
 
-	writeFile('build/index.html', templateOutput);
+  writeFile('build/index.html', templateOutput);
 };
 
 render();
