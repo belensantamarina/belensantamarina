fb50c22a3aae18794211c89c36e6b34a0bbb0731
Andi Moritan <andi.moritan@skyscanner.net>
2021-07-06 20:54:32 +0100

    gallery navegation added

--- script.js
+++ script.js
@@ -18,6 +18,7 @@ navButton.addEventListener('click', () => {
 
 const galleryContainer = document.getElementById('gallery');
 if (galleryContainer) {
+  const withNavigation = Boolean(galleryContainer.dataset.nav);
   const imageContainers = galleryContainer.querySelectorAll('li');
 
   const selectImage = (imageContainer) => {
@@ -27,8 +28,39 @@ if (galleryContainer) {
       );
     }
     imageContainer.classList.toggle('active');
+
+    if (withNavigation) {
+      const imageButtons = galleryContainer.querySelectorAll('button');
+      if (galleryContainer.dataset.selected) {
+        imageButtons[galleryContainer.dataset.selected].classList.toggle(
+          'active'
+        );
+      }
+      imageButtons[imageContainer.dataset.id].classList.toggle('active');
+    }
     galleryContainer.dataset.selected = imageContainer.dataset.id;
   };
+
+  if (withNavigation) {
+    const navigationContainer = document.createElement('div');
+    navigationContainer.setAttribute('aria-hidden', 'true');
+
+    imageContainers.forEach((imageContainer, index) => {
+      const imageButton = document.createElement('button');
+      imageButton.style.backgroundImage = `url('${imageContainer.children[0].getAttribute(
+        'src'
+      )}')`;
+      imageButton.addEventListener('click', (event) => {
+        clearInterval(autoPlayInterval);
+        selectImage(imageContainers[index]);
+      });
+      navigationContainer.appendChild(imageButton);
+    });
+
+    galleryContainer.appendChild(navigationContainer);
+  }
+
+  selectImage(imageContainers[0]);
 
   const autoPlayInterval = window.setInterval(() => {
     const currentId = Number(galleryContainer.dataset.selected);
@@ -36,6 +68,4 @@ if (galleryContainer) {
       imageContainers.length - 1 >= currentId + 1 ? currentId + 1 : 0;
     selectImage(imageContainers[nextId]);
   }, 3000);
-
-  selectImage(imageContainers[0]);
 }
