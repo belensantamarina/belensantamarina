b34b21bd0ef5448f5ad1e9fb25e937930f613e59
Andi Moritan <andi.moritan@skyscanner.net>
2021-07-06 18:55:42 +0100

    new gallery ui

--- script.js
+++ script.js
@@ -16,51 +16,26 @@ navButton.addEventListener('click', () => {
 /*						GALLERY						*/
 /********************************/
 
-const getContainerSourceAddress = (thumbnailSourceAddress) =>
-  thumbnailSourceAddress
-    .replace('thumbnail', window.innerWidth >= 1024 ? 'large' : 'small')
-    .replace(
-      '.jpg',
-      window.devicePixelRatio !== 1
-        ? `@${window.devicePixelRatio}x.jpg`
-        : '.jpg'
-    );
-
 const galleryContainer = document.getElementById('gallery');
 if (galleryContainer) {
-  const imageContainer = document.getElementById('img');
+  const imageContainers = galleryContainer.querySelectorAll('li');
 
-  const selectImage = (imageElement) => {
-    const thumbnailSource = imageElement.getAttribute('src');
-    const containerSource = getContainerSourceAddress(thumbnailSource);
-    imageContainer.style.backgroundImage = `url('${containerSource}')`;
-    imageContainer.dataset.selected = imageElement.dataset.id;
-
-    galleryContainer.querySelector('img.active')?.classList.toggle('active');
-    imageElement.classList.toggle('active');
+  const selectImage = (imageContainer) => {
+    if (galleryContainer.dataset.selected) {
+      imageContainers[galleryContainer.dataset.selected].classList.toggle(
+        'active'
+      );
+    }
+    imageContainer.classList.toggle('active');
+    galleryContainer.dataset.selected = imageContainer.dataset.id;
   };
 
-  const imageElements = galleryContainer.querySelectorAll('img');
-  selectImage(imageElements[0]);
-
   const autoPlayInterval = window.setInterval(() => {
-    const currentId = Number(imageContainer.dataset.selected);
+    const currentId = Number(galleryContainer.dataset.selected);
     const nextId =
-      imageElements.length - 1 >= currentId + 1 ? currentId + 1 : 0;
-    selectImage(imageElements[nextId]);
+      imageContainers.length - 1 >= currentId + 1 ? currentId + 1 : 0;
+    selectImage(imageContainers[nextId]);
   }, 3000);
 
-  imageElements.forEach((imageElement) => {
-    // Pre-fetch container-size assets
-    const tempImageElement = new Image();
-    tempImageElement.src = getContainerSourceAddress(
-      imageElement.getAttribute('src')
-    );
-
-    // Enable interactive navigation
-    imageElement.addEventListener('click', (event) => {
-      selectImage(event.target);
-      clearInterval(autoPlayInterval);
-    });
-  });
+  selectImage(imageContainers[0]);
 }
