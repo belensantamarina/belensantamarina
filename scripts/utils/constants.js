0b18019b4344463632ffd1ee4ebbd1ef34a607aa
Andi Moritan <andi.moritan@skyscanner.net>
2021-07-06 19:07:07 +0100

    update build scripts to reflect new media requirements

--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -17,27 +17,11 @@ const LANGUAGES = [
   },
 ];
 
-const PICTURE_SIZES = {
-  thumbnail: [
-    { tag: '', size: 40, density: 72 },
-    { tag: '@1.5x', size: 60, density: 96 },
-    { tag: '@2x', size: 80, density: 150 },
-    { tag: '@3x', size: 120, density: 300 },
-  ],
-  small: [
-    { tag: '', size: 320, density: 72 },
-    { tag: '@1.5x', size: 480, density: 96 },
-    { tag: '@2x', size: 640, density: 150 },
-    { tag: '@3x', size: 960, density: 300 },
-  ],
-  large: [
-    { tag: '', size: 480, density: 72 },
-    { tag: '@1.5x', size: 720, density: 96 },
-    { tag: '@2x', size: 960, density: 150 },
-    { tag: '@3x', size: 1440, density: 300 },
-  ],
-};
+const IMAGE_RESOLUTIONS = [
+  { tag: '', size: 480, density: 72 },
+  { tag: '@1.5x', size: 720, density: 96 },
+  { tag: '@2x', size: 960, density: 150 },
+  { tag: '@3x', size: 1440, density: 300 },
+];
 
-const IMAGE_EXTENSIONS = ['.png', '.jpeg', '.jpg', '.gif'];
-
-module.exports = { LANGUAGES, PICTURE_SIZES, IMAGE_EXTENSIONS };
+module.exports = { LANGUAGES, IMAGE_RESOLUTIONS };
