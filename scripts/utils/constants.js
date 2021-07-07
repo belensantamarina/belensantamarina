2a94f1c279746635070b4d7670327981e54c4a1f
Andi Moritan <andi.moritan@skyscanner.net>
2021-07-07 17:37:39 +0100

    new image sizes

--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -18,10 +18,10 @@ const LANGUAGES = [
 ];
 
 const IMAGE_RESOLUTIONS = [
-  { tag: '', size: 480, density: 72 },
-  { tag: '@1.5x', size: 720, density: 96 },
-  { tag: '@2x', size: 960, density: 150 },
-  { tag: '@3x', size: 1440, density: 300 },
+  { tag: '', width: 720, height: 320, density: 72 },
+  { tag: '@1.5x', width: 1080, height: 480, density: 96 },
+  { tag: '@2x', width: 1440, height: 640, density: 150 },
+  { tag: '@3x', width: 2160, height: 960, density: 300 },
 ];
 
 module.exports = { LANGUAGES, IMAGE_RESOLUTIONS };
