bfdf3e7ea6c81f8231622a73376ea541bbfb6001
Andi Moritan <amoritan@me.com>
2023-10-16 16:03:35 +0100

    Improve image resolution for new size

--- scripts/utils/constants.js
+++ scripts/utils/constants.js
@@ -37,10 +37,10 @@ const LANGUAGES = [
 ];
 
 const IMAGE_RESOLUTIONS = [
-  { tag: '', width: 720, height: 320, density: 72 },
-  { tag: '@1.5x', width: 1080, height: 480, density: 96 },
-  { tag: '@2x', width: 1440, height: 640, density: 150 },
-  { tag: '@3x', width: 2160, height: 960, density: 300 },
+  { tag: '', width: 720, height: 720, density: 72 },
+  { tag: '@1.5x', width: 1080, height: 1080, density: 96 },
+  { tag: '@2x', width: 1440, height: 1440, density: 150 },
+  { tag: '@3x', width: 2160, height: 2160, density: 300 },
 ];
 
 module.exports = { DOMAIN, LANGUAGES, IMAGE_RESOLUTIONS };
