91377d60ce3f4ee5260603747a1b76e53137bbb4
Andi Moritan <amoritan@me.com>
2024-07-14 12:16:08 +0100

    Script to process messages created

--- script.js
+++ script.js
@@ -144,3 +144,51 @@ if (navSocialContainer) {
     window.fetchSocialData();
   });
 }
+
+/********************************/
+/*           MESSAGES           */
+/********************************/
+
+const messagesForm = document.getElementById('messages');
+
+const obfuscateMessage = (message) => {
+  const validChars = /^[0-9A-Z:¡!¿?().";/]+$/;
+
+  const messageChars = message.toUpperCase().split('');
+  const messageCharsCount = messageChars.reduce((count, char) => {
+    if (validChars.test(char)) {
+      count[char] = (count[char] || 0) + 1;
+    }
+    return count;
+  }, {});
+
+  const messageCharsCountSorted = Object.entries(messageCharsCount).sort(
+    ([, valueA], [, valueB]) => valueB - valueA,
+  );
+
+  // const obfuscatedMessage = messageCharsCountSorted.reduce(
+  //   (countString, charArray) =>
+  //     `${countString}\n${charArray[0]}:${charArray[1]}`,
+  //   '',
+  // );
+  // return obfuscatedMessage;
+
+  return messageCharsCountSorted;
+};
+
+console.log(messagesForm);
+if (messagesForm) {
+  messagesForm.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    const messagesFormData = new FormData(messagesForm);
+    const messagesFormDataObject = Object.fromEntries(messagesFormData);
+
+    const sentMessage = {
+      ...messagesFormDataObject,
+      obfuscated: obfuscateMessage(messagesFormDataObject.message),
+    };
+
+    console.log(sentMessage);
+  });
+}
