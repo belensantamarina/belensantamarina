c7a839a83908314a5265043df22eb63901b21cd4
Andi Moritan <amoritan@me.com>
2024-07-14 17:50:39 +0100

    Changes to global script reverted

--- script.js
+++ script.js
@@ -7,12 +7,10 @@ const navButton = document.getElementById('nav');
 const navContainer = document.querySelector('nav');
 const headerContainer = document.querySelector('header');
 
-if (navButton) {
-  navButton.addEventListener('click', () => {
-    headerContainer.classList.toggle('active');
-    navContainer.classList.toggle('active');
-  });
-}
+navButton.addEventListener('click', () => {
+  headerContainer.classList.toggle('active');
+  navContainer.classList.toggle('active');
+});
 
 /********************************/
 /*						GALLERY						*/
@@ -83,8 +81,8 @@ if (galleryContainer) {
 /********************************/
 
 const navSocialContainer = document.querySelector('ul.social');
-let socialViewAllContainer;
-let socialUrl;
+const socialViewAllContainer = navSocialContainer.querySelector('li');
+const socialUrl = navSocialContainer.querySelector('a').href;
 
 const renderStatusInNav = ({ id, description, source }) => {
   const statusImage = document.createElement('img');
@@ -136,84 +134,6 @@ window.fetchSocialData = (
     });
 };
 
-if (navSocialContainer) {
-  socialViewAllContainer = navSocialContainer.querySelector('li');
-  socialUrl = navSocialContainer.querySelector('a').href;
-
-  window.addEventListener('load', () => {
-    window.fetchSocialData();
-  });
-}
-
-/********************************/
-/*           MESSAGES           */
-/********************************/
-
-const messagesForm = document.getElementById('messages');
-let messageTextarea;
-let messageCharCountSpan;
-
-const obfuscateMessage = (message) => {
-  const validChars = /^[0-9A-Z:¡!¿?().";/]+$/;
-
-  const messageChars = message
-    .toUpperCase()
-    .normalize('NFD')
-    .replace(/[\u0300-\u036f]/g, '')
-    .split('');
-  const messageCharsCount = messageChars.reduce((count, char) => {
-    if (validChars.test(char)) {
-      count[char] = (count[char] || 0) + 1;
-    }
-    return count;
-  }, {});
-
-  const messageCharsCountSorted = Object.entries(messageCharsCount).sort(
-    ([, valueA], [, valueB]) => valueB - valueA,
-  );
-
-  return messageCharsCountSorted;
-};
-
-console.log(messagesForm);
-if (messagesForm) {
-  messageTextarea = document.getElementById('message');
-  messageCharCountSpan = document.getElementById('messageCharCount');
-
-  const maxChars = 140;
-
-  messageTextarea.addEventListener('input', () => {
-    const nonSpaceChars = messageTextarea.value.replace(/\s/g, '');
-    if (nonSpaceChars.length > maxChars) {
-      let truncatedValue = '';
-      let count = 0;
-
-      for (let char of messageTextarea.value) {
-        if (char !== ' ' && char !== '\t' && char !== '\n' && char !== '\r') {
-          count++;
-        }
-        if (count > maxChars) {
-          break;
-        }
-        truncatedValue += char;
-      }
-      messageTextarea.value = truncatedValue;
-    }
-    const currentLength = messageTextarea.value.replace(/\s/g, '').length;
-    messageCharCountSpan.textContent = `${currentLength}/${maxChars}`;
-  });
-
-  messagesForm.addEventListener('submit', (event) => {
-    event.preventDefault();
-
-    const messagesFormData = new FormData(messagesForm);
-    const messagesFormDataObject = Object.fromEntries(messagesFormData);
-
-    const sentMessage = {
-      ...messagesFormDataObject,
-      obfuscated: obfuscateMessage(messagesFormDataObject.message),
-    };
-
-    console.log(sentMessage);
-  });
-}
+window.addEventListener('load', () => {
+  window.fetchSocialData();
+});
