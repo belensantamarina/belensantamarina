e759f1c5c577f1a8d286941c86b7996e39b4892c
Andi Moritan <amoritan@me.com>
2024-07-18 17:52:19 +0100

    Logic moved server side

--- functions/email.js
+++ functions/email.js
@@ -1,10 +1,32 @@
 export default async (request, context) => {
 
-  const json = await request.json();
+  const obfuscateMessage = (message) => {
+    const validChars = /^[0-9A-Z:¡!¿?().";/]+$/;
+
+    const messageChars = message
+      .toUpperCase()
+      .normalize('NFD')
+      .replace(/[\u0300-\u036f]/g, '')
+      .split('');
+    const messageCharsCount = messageChars.reduce((count, char) => {
+      if (validChars.test(char)) {
+        count[char] = (count[char] || 0) + 1;
+      }
+      return count;
+    }, {});
+
+    const messageCharsCountSorted = Object.entries(messageCharsCount).sort(
+      ([, valueA], [, valueB]) => valueB - valueA,
+    );
+
+    return messageCharsCountSorted;
+  };
+
+  const requestBody = await request.json();
 
   const response = JSON.stringify({
-    json,
-    context,
+    ...requestBody,
+    obfuscated: obfuscateMessage(requestBody.message),
   });
 
   return new Response(response);
--- messages.js
+++ messages.js
@@ -7,28 +7,6 @@ const messageTextarea = document.getElementById('message');
 const messageCharCountSpan = document.getElementById('messageCharCount');
 const maxChars = 140;
 
-// const obfuscateMessage = (message) => {
-//   const validChars = /^[0-9A-Z:¡!¿?().";/]+$/;
-
-//   const messageChars = message
-//     .toUpperCase()
-//     .normalize('NFD')
-//     .replace(/[\u0300-\u036f]/g, '')
-//     .split('');
-//   const messageCharsCount = messageChars.reduce((count, char) => {
-//     if (validChars.test(char)) {
-//       count[char] = (count[char] || 0) + 1;
-//     }
-//     return count;
-//   }, {});
-
-//   const messageCharsCountSorted = Object.entries(messageCharsCount).sort(
-//     ([, valueA], [, valueB]) => valueB - valueA,
-//   );
-
-//   return messageCharsCountSorted;
-// };
-
 messageTextarea.addEventListener('input', () => {
   const nonSpaceChars = messageTextarea.value.replace(/\s/g, '');
   if (nonSpaceChars.length > maxChars) {
@@ -54,17 +32,14 @@ const sendMessage = async (form) => {
   const formData = new FormData(form);
   const formDataObject = Object.fromEntries(formData);
 
-  console.log(formDataObject);
-
   try {
     const response = await fetch(form.action, {
       method: form.method,
       body: JSON.stringify(formDataObject),
     });
 
-    console.log(response);
-    const json = await response.json();
-    console.log(json);
+    const responseBody = await response.json();
+    console.log(responseBody);
 
   } catch (error) {
     console.error(error.message);
@@ -73,14 +48,6 @@ const sendMessage = async (form) => {
 
 messagesForm.addEventListener('submit', (event) => {
   event.preventDefault();
-
-  // const messagesFormData = new FormData(messagesForm);
-  // const messagesFormDataObject = Object.fromEntries(messagesFormData);
-
-  // const sentMessage = {
-  //   ...messagesFormDataObject,
-  //   obfuscated: obfuscateMessage(messagesFormDataObject.message),
-  // };
 
   sendMessage(event.target);
 });
